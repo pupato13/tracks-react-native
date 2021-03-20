04fa5e63ff69a6fdc132c0e9a70f9f79d24e02b7
Diego Pupato <pupato.diego@gmail.com>
2021-03-21 10:34:47 +1300

    chore: add react-navigation

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text
+                style={{ color: "#d72893", fontSize: 20, textAlign: "center" }}
+            >
+                Open up App.js to start working on your app!
+            </Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#1a154b",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
