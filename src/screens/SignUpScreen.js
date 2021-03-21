d8eb673633e7f1bb3372692056fdbbbdce8ddf88
Diego Pupato <pupato.diego@gmail.com>
2021-03-21 13:37:52 +1300

    refactor: create Sign Up screen layout

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,24 +1,35 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Button, Text, Input } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignUpScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Sign Up Screen</Text>
-            <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
-                <Text>go to Sign In</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("mainFlow")}>
-                <Text>go to Main Flow</Text>
-            </TouchableOpacity>
+            <Text h3 style={{ marginBottom: 24, textAlign: "center" }}>
+                Sign Up for Tracker
+            </Text>
+            <Spacer />
+            <Input label="Email" />
+            <Spacer />
+            <Input label="Password" secureTextEntry />
+            <Spacer />
+            <Button title="Sign Up" />
         </View>
     );
+};
+
+SignUpScreen.navigationOptions = () => {
+    return {
+        headerShown: false,
+    };
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16,
+        justifyContent: "center",
     },
     title: {
         fontSize: 24,
