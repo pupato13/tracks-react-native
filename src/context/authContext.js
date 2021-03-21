e401c63bf287c3c1332fac7f48a7a80d62887bc2
Diego Pupato <pupato.diego@gmail.com>
2021-03-21 18:46:34 +1300

    feat: add user token to AsyncStorage

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,35 +1,43 @@
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/trackerApi";
+import AsyncStorage from "@react-native-community/async-storage";
 
+const TOKEN_KEY = "@TrackersApp";
 const ACTIONS = {
     ADD_ERROR: "add_error",
+    SIGN_UP: "sign_up",
 };
 
 const authReducer = (state, action) => {
     switch (action.type) {
         case ACTIONS.ADD_ERROR:
             return { ...state, errorMessage: action.payload };
+        case ACTIONS.SIGN_UP:
+            return { token: action.payload, errorMessage: "" };
         default:
             return state;
     }
 };
 
-const signUp = (dispatch) => {
-    return async ({ email, password }) => {
-        try {
-            const response = await trackerApi.post("/signup", {
-                email,
-                password,
-            });
+const signUp = (dispatch) => async ({ email, password }) => {
+    try {
+        const response = await trackerApi.post("/signup", {
+            email,
+            password,
+        });
 
-            console.log(response.data);
-        } catch (error) {
-            dispatch({
-                type: ACTIONS.ADD_ERROR,
-                payload: "Something went wrong with Sign Up!",
-            });
-        }
-    };
+        await AsyncStorage.setItem(TOKEN_KEY, response.data.token);
+
+        dispatch({
+            type: ACTIONS.SIGN_UP,
+            payload: response.data.token,
+        });
+    } catch (error) {
+        dispatch({
+            type: ACTIONS.ADD_ERROR,
+            payload: "Something went wrong with Sign Up!",
+        });
+    }
 };
 
 const signIn = (dispatch) => {
@@ -48,7 +56,7 @@ export const { Provider, Context } = createDataContext(
     authReducer,
     { signUp, signIn, signOut },
     {
-        isSignedIn: false,
+        token: null,
         errorMessage: "",
     }
 );
