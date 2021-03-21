d1216cbd529fcf2423d0647b44b2a2bfeb7897fc
Diego Pupato <pupato.diego@gmail.com>
2021-03-21 13:43:10 +1300

    feat: add state to SignUp screen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Button, Text, Input } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignUpScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <View style={styles.container}>
             <Text h3 style={{ marginBottom: 24, textAlign: "center" }}>
                 Sign Up for Tracker
             </Text>
             <Spacer />
-            <Input label="Email" />
+            <Input
+                autoFocus={false}
+                autoCapitalize="none"
+                autoCorrect={false}
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+            />
             <Spacer />
-            <Input label="Password" secureTextEntry />
+            <Input
+                autoFocus={false}
+                autoCapitalize="none"
+                autoCorrect={false}
+                label="Password"
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+            />
             <Spacer />
             <Button title="Sign Up" />
         </View>
